3d5f96ca2faf95f73577425525ed29742db864fd
Abdulrazaq-pro <abdulrazmail@gmail.com>
2024-09-13 17:14:48 +0100

    first commit

--- app/Model1.js
+++ app/Model1.js
@@ -40,6 +40,39 @@ const IphoneModel = () => {
     </group>
   )
 }
+const IphoneModel1 = () => {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/Iphone15.glb')
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#three-canvas-container',
+        scrub: 1,
+        pin: true,
+        start: 'top top',
+        end: 'bottom top',
+      },
+    })
+
+    // Rotate the model along the Y-axis as the page scrolls
+    tl.to(group.current.rotation, { z: Math.PI, duration: 2 }) // Rotates 180 degrees on the Y axis
+  }, [])
+
+  return (
+    <group ref={group} dispose={null} scale={0.2} rotation={[Math.PI / 2, 0, 0]}>
+      <mesh geometry={nodes.M_Cameras.geometry} material={materials.cam} />
+      <mesh geometry={nodes.M_Glass.geometry} material={materials['glass.001']} />
+      <mesh geometry={nodes.M_Metal_Rough.geometry} material={materials.metal_rough} />
+      <mesh geometry={nodes.M_Metal_Shiny.geometry} material={materials.metal_Shiny} />
+      <mesh geometry={nodes.M_Plastic.geometry} material={materials.metal_rough} />
+      <mesh geometry={nodes.M_Portal.geometry} material={materials['M_Base.001']} />
+      <mesh geometry={nodes.M_Screen.geometry} material={materials.Screen} />
+      <mesh geometry={nodes.M_Speakers.geometry} material={materials.metal_rough} />
+      <mesh geometry={nodes.M_USB.geometry} material={materials.metal_rough} />
+    </group>
+  )
+}
 
 const Background = () => {
   const { scene } = useThree()
@@ -127,6 +160,13 @@ const App = () => (
         <IphoneModel />
         <ThreeScene /> {/* The component controlling the camera */}
         <Background />
+      </Canvas>
+    </div>
+    <div id="three-canvas-container" style={{ width: '100vw', height: '500px' }}>
+      <Canvas camera={{ position: [0, 0, 10], fov: 45 }} gl={{ antialias: true, alpha: false }}>
+      
+        <IphoneModel1 />
+    
       </Canvas>
     </div>
     <TextSection />
