173e6d22c52c167e17499a1c2279d11277774d60
Abdulrazaq-pro <abdulrazmail@gmail.com>
2024-09-13 13:42:59 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -10,7 +10,6 @@ export default function Home() {
     {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
       <Model1/>
       {/* <div>test</div> */}
-      
     </main>
     
   )
