11169ecaf071365c8eb0e803795f61d86e9139d9
Abdulrazaq-pro <abdulrazmail@gmail.com>
2024-09-13 16:19:12 +0100

    first commit

--- app/Model1.js
+++ app/Model1.js
@@ -1,80 +1,57 @@
-import React, { useRef, useEffect } from "react";
-import { Canvas, useThree } from "@react-three/fiber";
-import { useGLTF, OrbitControls } from "@react-three/drei";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import * as THREE from "three";
+import React, { useRef, useEffect } from 'react'
+import { Canvas, useThree } from '@react-three/fiber'
+import { useGLTF, OrbitControls } from '@react-three/drei'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import * as THREE from 'three'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 const IphoneModel = () => {
-  const group = useRef();
-  const { nodes, materials } = useGLTF("/Iphone15.glb");
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/Iphone15.glb')
 
   useEffect(() => {
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: "#three-canvas-container",
+        trigger: '#three-canvas-container',
         scrub: 1,
         //markers: true,
         pin: true,
-        start: "top top",
-        end: "bottom top",
-      },
-    });
+        start: 'top top',
+        end: 'bottom top'
+      }
+    })
 
-    tl.to(group.current.rotation, { z: Math.PI / 8, duration: 2 });
-  }, []);
+    tl.to(group.current.rotation, { z: Math.PI / 8, duration: 2 })
+  }, [])
 
   return (
-    <group
-      ref={group}
-      dispose={null}
-      scale={0.2}
-      rotation={[Math.PI / 2, 0, -Math.PI / 8]}
-    >
+    <group ref={group} dispose={null} scale={0.2} rotation={[Math.PI / 2, 0, -Math.PI / 8]}>
       <mesh geometry={nodes.M_Cameras.geometry} material={materials.cam} />
-      <mesh
-        geometry={nodes.M_Glass.geometry}
-        material={materials["glass.001"]}
-      />
-      <mesh
-        geometry={nodes.M_Metal_Rough.geometry}
-        material={materials.metal_rough}
-      />
-      <mesh
-        geometry={nodes.M_Metal_Shiny.geometry}
-        material={materials.metal_Shiny}
-      />
-      <mesh
-        geometry={nodes.M_Plastic.geometry}
-        material={materials.metal_rough}
-      />
-      <mesh
-        geometry={nodes.M_Portal.geometry}
-        material={materials["M_Base.001"]}
-      />
+      <mesh geometry={nodes.M_Glass.geometry} material={materials['glass.001']} />
+      <mesh geometry={nodes.M_Metal_Rough.geometry} material={materials.metal_rough} />
+      <mesh geometry={nodes.M_Metal_Shiny.geometry} material={materials.metal_Shiny} />
+      <mesh geometry={nodes.M_Plastic.geometry} material={materials.metal_rough} />
+      <mesh geometry={nodes.M_Portal.geometry} material={materials['M_Base.001']} />
       <mesh geometry={nodes.M_Screen.geometry} material={materials.Screen} />
-      <mesh
-        geometry={nodes.M_Speakers.geometry}
-        material={materials.metal_rough}
-      />
+      <mesh geometry={nodes.M_Speakers.geometry} material={materials.metal_rough} />
       <mesh geometry={nodes.M_USB.geometry} material={materials.metal_rough} />
     </group>
-  );
-};
+  )
+}
 
 const Background = () => {
-  const { scene } = useThree();
+  const { scene } = useThree()
   useEffect(() => {
-    scene.background = new THREE.Color("#555555");
-  }, [scene]);
+    scene.background = new THREE.Color('#555555')
+  }, [scene])
 
-  return null;
-};
+  return null
+}
 
 const TextSection = () => {
-  const textRefs = useRef([]);
+  const textRefs = useRef([])
 
   useEffect(() => {
     gsap.fromTo(
@@ -84,50 +61,42 @@ const TextSection = () => {
         opacity: 1,
         stagger: 0.1,
         scrollTrigger: {
-          trigger: "#text-trigger",
-          start: "top bottom",
-          end: "center center",
+          trigger: '#text-trigger',
+          start: 'top bottom',
+          end: 'center center',
           scrub: 1,
-          markers: false,
-        },
+          markers: false
+        }
       }
-    );
-  }, []);
+    )
+  }, [])
 
-  const texts = ["Ready 5", "Ready 4", "Ready 3", "Ready 2", "Ready 1"];
+  const texts = ['Ready 5', 'Ready 4', 'Ready 3', 'Ready 2', 'Ready 1']
 
   return (
     <div
       id="text-trigger"
       style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "relative",
-        top: "500px",
-      }}
-    >
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
+        top: '500px'
+      }}>
       {texts.map((text, index) => (
-        <h1
-          key={index}
-          ref={(el) => (textRefs.current[index] = el)}
-          style={{ opacity: 0 }}
-        >
+        <h1 key={index} ref={(el) => (textRefs.current[index] = el)} style={{ opacity: 0 }}>
           {text}
         </h1>
       ))}
     </div>
-  );
-};
+  )
+}
 
 const ThreeScene = () => (
-  <div id="three-canvas-container" style={{ width: "100vw", height: "500px" }}>
-    <Canvas
-      camera={{ position: [0, 0, 10], fov: 45 }}
-      gl={{ antialias: true, alpha: false }}
-    >
+  <div id="three-canvas-container" style={{ width: '100vw', height: '500px' }}>
+    <Canvas camera={{ position: [0, 0, 10], fov: 45 }} gl={{ antialias: true, alpha: false }}>
       <ambientLight intensity={0.4} />
       <directionalLight position={[5, 10, 7.5]} intensity={1} />
       <IphoneModel />
@@ -135,24 +104,16 @@ const ThreeScene = () => (
       <Background />
     </Canvas>
   </div>
-);
+)
 
-const Model1 = () => (
-  <div style={{ display: "flex", flexDirection: "column", height: "400vh" }}>
-    <div
-      className="some-content"
-      style={{
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+const App = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', height: '400vh' }}>
+    <div className="some-content" style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <h1>ACTION</h1>
     </div>
-    {/* <ThreeScene /> */}
+    <ThreeScene />
     <TextSection />
   </div>
-);
+)
 
-export default Model1;
+export default App
