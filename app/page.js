62943db0179c5379e09013a90dd68108de8bb814
Abdulrazaq-pro <146592510+Abdulrazaq-pro@users.noreply.github.com>
2024-09-14 12:26:39 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,9 +9,9 @@ export default function Home() {
   return (
     <main className="">
     {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
-      <Model1/>
+      <Model2/>
       
-      {/* <Model2/> */}
+      {/* <Model1/> */}
       
     </main>
     
